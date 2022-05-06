1eeda2b401366aecc8b1ffe32b6fee88092a7db1
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-06 08:56:34 -0300

    setting packages for advanced_form for all kind of users

---
+++ src/Rejister.js
@@ -0,0 +1,11 @@
+import { useRef, useEffect, useState } from 'react';
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const Rejister = () => {
+  return <div>Rejister</div>;
+};
