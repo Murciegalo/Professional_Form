4ba3af3668e42c15d3342bd91f982c1350207be8
Murciegalo <39123379+Murciegalo@users.noreply.github.com>
2022-05-09 15:06:23 -0300

    Login comp. clientSide in progress

---
+++ src/Login.js
@@ -0,0 +1,71 @@
+import { useRef, useState, useEffect } from 'react';
+
+export const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [pwd, setPwd] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    // userRef.current.focus()
+  }, []);
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSuccess('true');
+  };
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>Success</h1>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? 'errmsg' : 'offscreen'}
+            aria-live='assertive'>
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username:</label>
+            <input
+              type='text'
+              id='username'
+              ref={userRef}
+              autoComplete='off'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              required
+            />
+            <label htmlFor='password'>Password:</label>
+            <input
+              type='password'
+              id='password'
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button>Sign In</button>
+          </form>
+          <p>
+            Need an account?
+            <br />
+            <span className='line'>
+              {/* react-link here */}
+              <a href='#'>Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
+};
